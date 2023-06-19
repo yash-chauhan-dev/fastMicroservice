4b426be523a34e1b067fee19397140631eaaff33
Yash Chauhan <y4shchauhan@gmail.com>
2023-06-20 01:44:18 +0530

    inventory UI done

---
+++ app/inventory-frontend/src/App.js
@@ -0,0 +1,162 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App" data-bs-theme="dark" style={{ backgroundColor: "rgba(255, 0, 0, 0.5)", overflow: 'hidden', height: '100vh' }}>
+      <header class="text-bg-dark py-4 mb-1 border-bottom" style={{ height: '10vh' }}>
+        <div class="container">
+          <div class="text-center">
+            <a href="/" class="text-white text-decoration-none">
+              Company</a>
+          </div>
+        </div>
+      </header>
+      <main class="px-md-1 text-bg-dark" style={{ height: '80vh', overflowY: 'scroll' }}>
+        <table class="table table-striped table-sm" style={{ position: "sticky", top: "0" }}>
+          <thead>
+            <tr>
+              <th scope="col" style={{ position: "sticky", top: "0" }}>#</th>
+              <th scope="col" style={{ position: "sticky", top: "0" }}>Header</th>
+              <th scope="col" style={{ position: "sticky", top: "0" }}>Header</th>
+              <th scope="col" style={{ position: "sticky", top: "0" }}>Header</th>
+              <th scope="col" style={{ position: "sticky", top: "0" }}>Header</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1,001</td>
+              <td>random</td>
+              <td>data</td>
+              <td>placeholder</td>
+              <td>text</td>
+            </tr>
+            <tr>
+              <td>1,002</td>
+              <td>placeholder</td>
+              <td>irrelevant</td>
+              <td>visual</td>
+              <td>layout</td>
+            </tr>
+            <tr>
+              <td>1,012</td>
+              <td>text</td>
+              <td>placeholder</td>
+              <td>layout</td>
+              <td>dashboard</td>
+            </tr>
+            <tr>
+              <td>1,001</td>
+              <td>random</td>
+              <td>data</td>
+              <td>placeholder</td>
+              <td>text</td>
+            </tr>
+            <tr>
+              <td>1,002</td>
+              <td>placeholder</td>
+              <td>irrelevant</td>
+              <td>visual</td>
+              <td>layout</td>
+            </tr>
+            <tr>
+              <td>1,012</td>
+              <td>text</td>
+              <td>placeholder</td>
+              <td>layout</td>
+              <td>dashboard</td>
+            </tr>
+            <tr>
+              <td>1,001</td>
+              <td>random</td>
+              <td>data</td>
+              <td>placeholder</td>
+              <td>text</td>
+            </tr>
+            <tr>
+              <td>1,002</td>
+              <td>placeholder</td>
+              <td>irrelevant</td>
+              <td>visual</td>
+              <td>layout</td>
+            </tr>
+            <tr>
+              <td>1,012</td>
+              <td>text</td>
+              <td>placeholder</td>
+              <td>layout</td>
+              <td>dashboard</td>
+            </tr>
+            <tr>
+              <td>1,001</td>
+              <td>random</td>
+              <td>data</td>
+              <td>placeholder</td>
+              <td>text</td>
+            </tr>
+            <tr>
+              <td>1,002</td>
+              <td>placeholder</td>
+              <td>irrelevant</td>
+              <td>visual</td>
+              <td>layout</td>
+            </tr>
+            <tr>
+              <td>1,012</td>
+              <td>text</td>
+              <td>placeholder</td>
+              <td>layout</td>
+              <td>dashboard</td>
+            </tr>
+            <tr>
+              <td>1,001</td>
+              <td>random</td>
+              <td>data</td>
+              <td>placeholder</td>
+              <td>text</td>
+            </tr>
+            <tr>
+              <td>1,002</td>
+              <td>placeholder</td>
+              <td>irrelevant</td>
+              <td>visual</td>
+              <td>layout</td>
+            </tr>
+            <tr>
+              <td>1,012</td>
+              <td>text</td>
+              <td>placeholder</td>
+              <td>layout</td>
+              <td>dashboard</td>
+            </tr>
+            <tr>
+              <td>1,001</td>
+              <td>random</td>
+              <td>data</td>
+              <td>placeholder</td>
+              <td>text</td>
+            </tr>
+            <tr>
+              <td>1,002</td>
+              <td>placeholder</td>
+              <td>irrelevant</td>
+              <td>visual</td>
+              <td>layout</td>
+            </tr>
+            <tr>
+              <td>1,012</td>
+              <td>text</td>
+              <td>placeholder</td>
+              <td>layout</td>
+              <td>dashboard</td>
+            </tr>
+          </tbody>
+        </table>
+      </main >
+      <footer class="text-bg-dark my-1 py-4" style={{ height: '10vh' }}>
+        <p class="text-center">&copy; 2023 Company, Inc</p>
+      </footer>
+    </div >
+  );
+}
+
+export default App;
